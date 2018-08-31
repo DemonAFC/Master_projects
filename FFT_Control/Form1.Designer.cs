4d3748fd96a806754a80269919b8797542f60ae8
DemonAFC <hoang.pham.fet.hut@gmail.com>
2018-08-31 20:28:21 +0100

    FFT_Control
    The PC-based application control data flow between PC and Basy3 board

---
+++ FFT_Control/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace uart_ex3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bConnect = new System.Windows.Forms.Button();
+            this.bClose = new System.Windows.Forms.Button();
+            this.bSend = new System.Windows.Forms.Button();
+            this.tbTx = new System.Windows.Forms.TextBox();
+            this.tbRX = new System.Windows.Forms.RichTextBox();
+            this.cPort = new System.Windows.Forms.ComboBox();
+            this.cBaudrate = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbEX = new System.Windows.Forms.RichTextBox();
+            this.bImport = new System.Windows.Forms.Button();
+            this.pBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // bConnect
+            // 
+            this.bConnect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bConnect.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.bConnect.Location = new System.Drawing.Point(284, 275);
+            this.bConnect.Name = "bConnect";
+            this.bConnect.Size = new System.Drawing.Size(101, 24);
+            this.bConnect.TabIndex = 0;
+            this.bConnect.Text = "Connect";
+            this.bConnect.UseVisualStyleBackColor = true;
+            this.bConnect.Click += new System.EventHandler(this.bConnect_Click);
+            // 
+            // bClose
+            // 
+            this.bClose.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bClose.Location = new System.Drawing.Point(419, 274);
+            this.bClose.Name = "bClose";
+            this.bClose.Size = new System.Drawing.Size(101, 24);
+            this.bClose.TabIndex = 1;
+            this.bClose.Text = "Close";
+            this.bClose.UseVisualStyleBackColor = true;
+            this.bClose.Click += new System.EventHandler(this.bClose_Click);
+            // 
+            // bSend
+            // 
+            this.bSend.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bSend.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.bSend.Location = new System.Drawing.Point(419, 312);
+            this.bSend.Name = "bSend";
+            this.bSend.Size = new System.Drawing.Size(101, 24);
+            this.bSend.TabIndex = 2;
+            this.bSend.Text = "Send";
+            this.bSend.UseVisualStyleBackColor = true;
+            this.bSend.Click += new System.EventHandler(this.bSend_Click);
+            this.bSend.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbTx_KeyDown);
+            // 
+            // tbTx
+            // 
+            this.tbTx.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbTx.Location = new System.Drawing.Point(16, 312);
+            this.tbTx.Name = "tbTx";
+            this.tbTx.Size = new System.Drawing.Size(234, 22);
+            this.tbTx.TabIndex = 3;
+            this.tbTx.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbTx_KeyDown);
+            // 
+            // tbRX
+            // 
+            this.tbRX.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbRX.Location = new System.Drawing.Point(12, 17);
+            this.tbRX.Name = "tbRX";
+            this.tbRX.Size = new System.Drawing.Size(238, 228);
+            this.tbRX.TabIndex = 4;
+            this.tbRX.Text = "";
+            this.tbRX.TextChanged += new System.EventHandler(this.tbRX_TextChanged);
+            // 
+            // cPort
+            // 
+            this.cPort.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cPort.FormattingEnabled = true;
+            this.cPort.Location = new System.Drawing.Point(15, 276);
+            this.cPort.Name = "cPort";
+            this.cPort.Size = new System.Drawing.Size(101, 24);
+            this.cPort.TabIndex = 5;
+            // 
+            // cBaudrate
+            // 
+            this.cBaudrate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cBaudrate.FormattingEnabled = true;
+            this.cBaudrate.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600"});
+            this.cBaudrate.Location = new System.Drawing.Point(149, 275);
+            this.cBaudrate.Name = "cBaudrate";
+            this.cBaudrate.Size = new System.Drawing.Size(101, 24);
+            this.cBaudrate.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Baudrate";
+            // 
+            // tbEX
+            // 
+            this.tbEX.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbEX.Location = new System.Drawing.Point(284, 17);
+            this.tbEX.Name = "tbEX";
+            this.tbEX.Size = new System.Drawing.Size(236, 228);
+            this.tbEX.TabIndex = 12;
+            this.tbEX.Text = "";
+            this.tbEX.TextChanged += new System.EventHandler(this.tbEX_TextChanged);
+            // 
+            // bImport
+            // 
+            this.bImport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bImport.Location = new System.Drawing.Point(284, 312);
+            this.bImport.Name = "bImport";
+            this.bImport.Size = new System.Drawing.Size(101, 24);
+            this.bImport.TabIndex = 13;
+            this.bImport.Text = "Import";
+            this.bImport.UseVisualStyleBackColor = true;
+            this.bImport.Click += new System.EventHandler(this.bImport_Click);
+            // 
+            // pBar
+            // 
+            this.pBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pBar.Location = new System.Drawing.Point(284, 255);
+            this.pBar.Name = "pBar";
+            this.pBar.Size = new System.Drawing.Size(236, 10);
+            this.pBar.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 352);
+            this.Controls.Add(this.pBar);
+            this.Controls.Add(this.bImport);
+            this.Controls.Add(this.tbEX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cBaudrate);
+            this.Controls.Add(this.cPort);
+            this.Controls.Add(this.tbRX);
+            this.Controls.Add(this.tbTx);
+            this.Controls.Add(this.bSend);
+            this.Controls.Add(this.bClose);
+            this.Controls.Add(this.bConnect);
+            this.MaximumSize = new System.Drawing.Size(551, 1000);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "FFT Control";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bConnect;
+        private System.Windows.Forms.Button bClose;
+        private System.Windows.Forms.Button bSend;
+        private System.Windows.Forms.TextBox tbTx;
+        private System.Windows.Forms.RichTextBox tbRX;
+        private System.Windows.Forms.ComboBox cPort;
+        private System.Windows.Forms.ComboBox cBaudrate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox tbEX;
+        private System.Windows.Forms.Button bImport;
+        private System.Windows.Forms.ProgressBar pBar;
+    }
+}
+
---
+++ FFT_Control/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uart_ex3
+{
+    public partial class Form1 : Form
+    {
+        static List<string> queue = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            bClose.Enabled = false;
+            foreach(String s in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                cPort.Items.Add(s);    
+            }
+        }
+
+        public System.IO.Ports.SerialPort sport;    
+
+        public void serialport_connect(String port, int baudrate, Parity parity, int databits, StopBits stopbits)
+        {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToShortTimeString();
+
+            sport = new System.IO.Ports.SerialPort(port, baudrate, parity, databits, stopbits);
+            try
+            {
+                sport.Open();
+                bClose.Enabled = true;
+                bConnect.Enabled = false;
+                tbRX.AppendText("[" + dtn + "]" + " Connected.\n");
+
+                sport.DataReceived += new SerialDataReceivedEventHandler(sport_Datareceived);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), " Error!");
+            }
+        }
+
+        private void sport_Datareceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToShortTimeString();
+            int n;
+            bool IsExport_result = false;
+            bool IsExport_input = false;
+            var data = new List<byte>();
+            var export_result = new List<byte>();
+            var export_input = new List<byte>();
+            do
+            {
+                n = sport.BytesToRead;
+                byte[] buffer = new byte[n];
+                try
+                {
+                    if (cmd_send == "5" && cmd_send_last != "1")
+                    {
+                        var size = sport.Read(buffer, 0, buffer.Length);
+                        export_input.AddRange(buffer);
+                    }
+                    else if (cmd_send == "4" && cmd_send_last != "1")
+                    {
+                        var size = sport.Read(buffer, 0, buffer.Length);
+                        export_result.AddRange(buffer);       
+                    }
+                    else
+                    {
+                        var size = sport.Read(buffer, 0, buffer.Length);
+                        data.AddRange(buffer);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), " Error!");
+                }
+            } while (n > 0);
+
+            String exportdata_input = Encoding.ASCII.GetString(export_input.ToArray());
+            String exportdata_result = Encoding.ASCII.GetString(export_result.ToArray());
+            String recivedata = Encoding.ASCII.GetString(data.ToArray());
+            tbEX.AppendText(exportdata_input);
+            tbEX.AppendText(exportdata_result);
+            tbRX.AppendText(recivedata);
+
+            if (cmd_send == "5" && cmd_send_last != "1") // Export result FFT
+            {
+                StreamWriter File = new StreamWriter("exportdata_input.txt");
+                File.Write(tbEX.Text);
+                File.Close();
+            }
+            if (IsExport_input && n == 0)
+            {
+                tbRX.AppendText("[" + dtn + "]" + " exportdata_result done.\n");
+            }
+
+            if (cmd_send == "4" && cmd_send_last != "1") // Export result FFT
+            {
+                StreamWriter File = new StreamWriter("exportdata_result.txt");
+                File.Write(tbEX.Text);
+                File.Close();
+       
+            }
+            if (IsExport_result)
+            {
+                tbRX.AppendText("[" + dtn + "]" + " exportdata_result done.\n");
+            }
+        }     
+
+        private void bConnect_Click(object sender, EventArgs e)
+        {
+            String port = cPort.Text;
+            int baudrate = Convert.ToInt32(cBaudrate.Text);
+            if (port == "")
+            {
+                serialport_connect("COM5", 9600, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One); // for debug
+            }
+            else
+            {
+                serialport_connect(port, Convert.ToInt32(cBaudrate.Text), System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One); // for run
+            }
+
+
+        }
+
+        string[] Import = new string[1024];
+        String cmd_send;
+        String cmd_send_last;
+        private void bSend_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToShortTimeString();
+            cmd_send_last = cmd_send;
+            cmd_send = tbTx.Text;
+            
+            pBar.Maximum = Import.Length;
+            pBar.Step = 1;
+            tbEX.Clear();
+
+            sport.Write(cmd_send);
+            tbRX.AppendText("\n---------------------------------------------\n\n");
+            tbRX.AppendText("[" + dtn + "]" + " Sent: " + cmd_send + "\n\n");
+                
+            // Import new data on board
+            if (cmd_send == "3" && IsImport)
+            {     
+                for (int i = 0; i < Import.Length; i++)
+                {
+                    sport.Write(Import[i]);
+                    System.Threading.Thread.Sleep(10); // Debug transimiting error
+                    sport.Write("\n");
+                    System.Threading.Thread.Sleep(5);
+                    pBar.PerformStep();
+                }
+                sport.Write("\r");
+                tbRX.AppendText("[" + dtn + "]" + " Import done.\n");
+            }
+        }
+
+        private void bClose_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToShortTimeString();
+
+            if (sport.IsOpen)
+            {
+                sport.Close();
+                bClose.Enabled = false;
+                bConnect.Enabled = true;
+                tbRX.AppendText("[" + dtn + "]" + " Disconnected.\n");
+            }
+        }
+
+        private void tbTx_KeyDown(object sender, KeyEventArgs e)
+        {
+                if (e.KeyCode == Keys.Return)
+                {
+                    bSend.PerformClick();
+                    // these last two lines will stop the beep sound
+                    e.SuppressKeyPress = true;
+                    e.Handled = true;
+                }
+            tbTx.Clear();
+        }
+
+        private void tbRX_TextChanged(object sender, EventArgs e)
+        {
+            // set the current caret position to the end
+            tbRX.SelectionStart = tbRX.Text.Length;
+            // scroll it automatically
+            tbRX.ScrollToCaret();
+        }
+
+        private void tbEX_TextChanged(object sender, EventArgs e)
+        {
+            // set the current caret position to the end
+            tbEX.SelectionStart = tbEX.Text.Length;
+            // scroll it automatically
+            tbEX.ScrollToCaret();
+        }
+
+        bool IsImport = false;
+        private void bImport_Click(object sender, EventArgs e)
+        {
+            pBar.Value = 0; // reset value of progress bar
+            tbEX.Clear(); // clear the export window
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents|*.txt", Multiselect = false, ValidateNames = true })
+                {
+                    if(ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        Import = System.IO.File.ReadAllLines(ofd.FileName);
+                        for(int i=0; i< Import.Length; i++)
+                        {
+                            tbEX.AppendText(Import[i]+"\n");
+                        }
+                    }
+                }
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), " Error!");
+            }
+            IsImport = true;
+        }
+    }
+}
